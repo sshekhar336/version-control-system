497ea3b5120abec81d694cd906bbe80d7dd97c2c
Shashank Shekhar <shashank@devkraft.in>
2020-09-15 14:21:43 +0530

    Updates

---
+++ precache-manifest.60598b4989fabdb6dc6ab723432d1c2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aebf161a6bcacea2bcfd611da7550410",
+    "url": "/version-control-system/index.html"
+  },
+  {
+    "revision": "6a85fb69d5d2ca89d483",
+    "url": "/version-control-system/static/css/main.350d312f.chunk.css"
+  },
+  {
+    "revision": "5f6530302832e3527881",
+    "url": "/version-control-system/static/js/2.c0295d78.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/version-control-system/static/js/2.c0295d78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a85fb69d5d2ca89d483",
+    "url": "/version-control-system/static/js/main.a1eb23ff.chunk.js"
+  },
+  {
+    "revision": "82a49f87f5743b54bc6f",
+    "url": "/version-control-system/static/js/runtime-main.f3d11c92.js"
+  }
+]);
